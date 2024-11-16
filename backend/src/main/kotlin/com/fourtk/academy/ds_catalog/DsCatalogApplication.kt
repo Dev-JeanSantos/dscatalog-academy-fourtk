2dd725b07df3d3c2f7f025a05e4b05d264fd3eb8
Dev-JeanSantos <jeancbsan@gmail.com>
2024-11-16 20:24:51 -0300

    Iniciando o projeto

---
+++ backend/src/main/kotlin/com/fourtk/academy/ds_catalog/DsCatalogApplication.kt
@@ -0,0 +1,11 @@
+package com.fourtk.academy.ds_catalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsCatalogApplication
+
+fun main(args: Array<String>) {
+	runApplication<DsCatalogApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/fourtk/academy/ds_catalog/DsCatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fourtk.academy.ds_catalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
