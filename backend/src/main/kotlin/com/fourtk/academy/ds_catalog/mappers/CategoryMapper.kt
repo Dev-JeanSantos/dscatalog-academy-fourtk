bf939cdacf81858c2239a60ec4af00e9f199e118
Dev-JeanSantos <jeancbsan@gmail.com>
2024-11-18 13:43:00 -0300

    feature: ajustes properties jpa e retirada do warn do mapper

--- backend/src/main/kotlin/com/fourtk/academy/ds_catalog/mappers/CategoryMapper.kt
+++ backend/src/main/kotlin/com/fourtk/academy/ds_catalog/mappers/CategoryMapper.kt
@@ -3,9 +3,11 @@ package com.fourtk.academy.ds_catalog.mappers
 import com.fourtk.academy.ds_catalog.domain.Category
 import com.fourtk.academy.ds_catalog.dtos.requests.CategoryRequestDTO
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring")
 interface CategoryMapper {
+    @Mapping(target = "id", ignore = true)
     fun toRequestDTOEntity(request: CategoryRequestDTO): Category
 
 }
